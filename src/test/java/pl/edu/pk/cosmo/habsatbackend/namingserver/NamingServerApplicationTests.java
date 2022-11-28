93ea9628aa2794efca5402b413c001582e18272e
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2022-11-28 13:27:33 +0100

    initial commit made with :carrot: & :hearts:

---
+++ src/test/java/pl/edu/pk/cosmo/habsatbackend/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pk.cosmo.habsatbackend.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
